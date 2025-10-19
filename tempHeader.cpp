444455b5daafc3633164ceaeea715f6e1bac2281
GaneshBalaji-04 <ganeshbalajiv2004@gmail.com>
2025-10-19 22:11:26 +0530

    New learning with templates and headers

---
+++ tempHeader.cpp
@@ -0,0 +1,9 @@
+// Its a sample file to learn how header and template works. I'll commit today and delete it once I got somewhat familiar with templates and header files
+
+#include "header.h"
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<add(5,6)<<endl;
+}
