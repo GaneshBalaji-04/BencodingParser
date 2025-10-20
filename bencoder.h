40fb54c2dc7c141cad66051a871dd6613cc6243e
GaneshBalaji-04 <balajibarani2004@protonmail.com>
2025-10-20 05:55:39 +0530

    Right now, I have included integer and string encoding in bencode.h. I'll improve and correct errors if present.

---
+++ bencoder.h
@@ -0,0 +1,44 @@
+#ifndef __BENCODER_H_INCLUDED__
+#define __BENCODER_H_INCLUDED__
+
+#include<bits/stdc++.h>
+
+string bencode(int &a){
+    string val = to_string(a);
+    string res = 'i' + val + 'e';
+
+    return res;
+}
+
+string bencodehelper(string &a){
+    return to_string(a.size()) + ':' + a;
+}
+
+string bencode(string &a){
+    string digit = "", strs = "";
+    string ans;
+    for(int i=0; i<a.size(); i++){
+        if(isdigit(s[i]) and strs == ""){
+            digit+=s[i];
+        }
+        else if(isdigit(s[i]) and strs != ""){
+            string strans = bencodehelper(strs);
+            ans+=strans;
+            strs = "";
+            digit+=s[i];
+        }
+        else if(!isdigit(s[i]) and digit == ""){
+            strs+=s[i];
+        }
+        else if(!isdigit(s[i]) and digit != ""){
+            int value = stoi(digit);
+            string intans = bencode(value);
+            ans+=intans;
+            digit = "";
+            strs+=s[i];
+        }
+    }
+    return ans;
+}
+
+#endif
